5adb77463a1b72b94293e4fcaa5ebccf32f2388d
mrakhaf <muhammadrakhaf@gmail.com>
2020-04-21 18:36:32 +0700

    aba

---
+++ src/script/view/main.js
@@ -0,0 +1,26 @@
+import '../component/club-list.js';
+import DataSource from '../data/data-source.js';
+
+const main = () => {
+    const searchElement = document.querySelector("search-bar");
+
+    const clubListElement = document.querySelector("club-list");
+
+    const onButtonSearchClicked = () => {
+        DataSource.searchClub(searchElement.value)
+            .then(renderResult)
+            .then(fallbackResult)
+    };
+
+    const renderResult = (results) => {
+        clubListElement.clubs = results;
+    };
+
+    const fallbackResult = (message) => {
+        clubListElement.renderError(message);
+    };
+
+    searchElement.clickEvent = onButtonSearchClicked;
+};
+
+export default main;
